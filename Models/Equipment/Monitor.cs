5f445e922ba8e9f5cfa4c58c645af0e18a792816
Tomasz <s30650@pjwstk.edu.pl>
2026-03-23 12:23:11 +0100

    Add Monitor class

---
+++ Models/Equipment/Monitor.cs
@@ -0,0 +1,9 @@
+﻿namespace APBD_Cw1_s30650.Models.Equipment;
+
+public class Monitor(string brand, string model, int screenSize, string resolution, int refreshRate, string panelType, decimal price) : Equipment(brand, model, price)
+{
+    public int ScreenSize { get; } = screenSize;
+    public string Resolution { get; } = resolution;
+    public int RefreshRate { get; } = refreshRate;
+    public string PanelType { get; } = panelType;
+}

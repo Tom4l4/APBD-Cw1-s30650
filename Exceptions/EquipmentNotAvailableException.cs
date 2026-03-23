8a05159ed007ae161d5f88d5d6406c76560a6f0f
Tomasz <s30650@pjwstk.edu.pl>
2026-03-23 13:36:38 +0100

    Add RentalService and EquipmentNotAvailableException

---
+++ Exceptions/EquipmentNotAvailableException.cs
@@ -0,0 +1,3 @@
+﻿namespace APBD_Cw1_s30650.Exceptions;
+
+public class EquipmentNotAvailableException(int equimpentId) : Exception($"Equipment with id {equimpentId} is not available");
---
+++ Services/RentalService.cs
@@ -0,0 +1,22 @@
+﻿using APBD_Cw1_s30650.Models.Rentals;
+using APBD_Cw1_s30650.Models.Users;
+using APBD_Cw1_s30650.Models.Equipments;
+using APBD_Cw1_s30650.Enums;
+using APBD_Cw1_s30650.Exceptions;
+
+namespace APBD_Cw1_s30650.Services;
+
+public class RentalService
+{
+    private List<Rental> rentals = new();
+    
+    public void RentEquipment(User user, Equipment equipment, int days)
+    {
+        if (equipment.Status != EquipmentStatus.Available)
+        {
+            throw new EquipmentNotAvailableException(equipment.Id);
+        }
+    }
+    
+}
+

a5ed22df1c16fdf31d1a1e076755ef3d75a35d5f
Tomasz <s30650@pjwstk.edu.pl>
2026-03-23 12:30:32 +0100

    Rename Router class and property naming

--- Models/Equipment/Rotuer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace APBD_Cw1_s30650.Models.Equipment;
-
-public class Rotuer(string brand, string model, string wifiStandard, int maxSpeed, int numberPorts, decimal price) : Equipment(brand, model, price)
-{
-    public string WifiStandard { get; } = wifiStandard;
-    public int MaxSpeed { get; } = maxSpeed;
-    public int NumberPorts { get; } = numberPorts;
-}

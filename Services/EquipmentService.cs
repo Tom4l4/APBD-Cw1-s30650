326b52e405d7aeb76c48581255767112a8bc9e86
Tomasz <s30650@pjwstk.edu.pl>
2026-03-23 14:55:02 +0100

    Add EquipmentService class

---
+++ Services/EquipmentService.cs
@@ -0,0 +1,24 @@
+﻿using APBD_Cw1_s30650.Enums;
+using APBD_Cw1_s30650.Models.Equipments;
+
+namespace APBD_Cw1_s30650.Services;
+
+public class EquipmentService
+{
+    private List<Equipment> equipments = new();
+
+    public void AddEquipment(Equipment equipment)
+    {
+        equipments.Add(equipment);
+    }
+
+    public List<Equipment> GetAllEquipments()
+    {
+        return equipments;
+    }
+
+    public List<Equipment> GetAvailableEquipments()
+    {
+        return equipments.Where(e => e.Status == EquipmentStatus.Available).ToList();
+    }
+}

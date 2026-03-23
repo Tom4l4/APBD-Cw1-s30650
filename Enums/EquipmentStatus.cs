75564a02befe4ecf1537fe75450eb9f6f39272d6
Tomasz <s30650@pjwstk.edu.pl>
2026-03-23 11:08:23 +0100

    Add EquipmentStatus enum

---
+++ Enums/EquipmentStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace APBD_Cw1_s30650.Enums;
+
+public enum EquipmentStatus
+{
+    Available,
+    Unavaible
+}
---
+++ Models/Equipment.cs
@@ -0,0 +1,12 @@
+﻿namespace APBD_Cw1_s30650.Models;
+
+public class Equipment
+{
+    private static int _nextId = 1;
+
+    public int Id { get; } = _nextId++;
+    public string Brand { get; set; } = brand;
+    public string Model { get; set; } = model;
+    public  Status { get; set; } = status;
+    
+}

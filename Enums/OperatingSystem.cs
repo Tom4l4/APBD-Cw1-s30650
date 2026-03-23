ed07775213f2a9d645a15e65b9c4e8bc5f4f9437
Tomasz <s30650@pjwstk.edu.pl>
2026-03-23 11:57:17 +0100

    Add Laptop class and operating system enum

---
+++ Enums/OperatingSystem.cs
@@ -0,0 +1,9 @@
+﻿namespace APBD_Cw1_s30650.Enums;
+
+public enum OperatingSystem
+{
+    Windows,
+    Ubuntu,
+    Debian,
+    MacOs
+}
---
+++ Models/Equipment/Laptop.cs
@@ -0,0 +1,9 @@
+﻿namespace APBD_Cw1_s30650.Models.Equipment;
+
+public class Laptop(string brand, string model, int diskspace, int memory, string processor, OperatingSystem system ,decimal price) : Equipment(brand, model, price)
+{
+    public int DiskSpace { get; set; } = diskspace;
+    public int Memory { get; set; } = memory;
+    public string Processor { get; set; } = processor;
+    public OperatingSystem System { get; set; } = system;
+}

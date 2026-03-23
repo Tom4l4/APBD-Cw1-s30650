562eb30e9928382ffbc5caa60153a178d8e8c56a
Tomasz <s30650@pjwstk.edu.pl>
2026-03-23 14:48:36 +0100

    Add UserService class

---
+++ Services/UserService.cs
@@ -0,0 +1,18 @@
+﻿using APBD_Cw1_s30650.Models.Users;
+
+namespace APBD_Cw1_s30650.Services;
+
+public class UserService
+{
+    private List<User> users = new();
+
+    public void AddUser(User user)
+    {
+        users.Add(user);
+    }
+
+    public List<User> GetAllUsers()
+    {
+        return users;
+    }
+}

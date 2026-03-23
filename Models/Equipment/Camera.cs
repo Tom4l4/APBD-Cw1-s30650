f5dc185a4a9e4c8bda6647272b651bcb93c66812
Tomasz <s30650@pjwstk.edu.pl>
2026-03-23 12:18:49 +0100

    Add Camera class

---
+++ Models/Equipment/Camera.cs
@@ -0,0 +1,8 @@
+﻿namespace APBD_Cw1_s30650.Models.Equipment;
+
+public class Camera(string brand, string model, string resolution, int opticalZoom, int frameRate, decimal price) : Equipment(brand, model, price)
+{
+    public string Resolution { get; } = resolution;
+    public int OpticalZoom { get; } = opticalZoom;
+    public int FrameRate { get; } = frameRate;
+}

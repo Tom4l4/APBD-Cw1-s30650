434467e9b8b738bf9826a80550555be81f988296
Tomasz <s30650@pjwstk.edu.pl>
2026-03-23 12:14:02 +0100

    Add Projector class and correction in Laptop class

--- Models/Equipment/Laptop.cs
+++ Models/Equipment/Laptop.cs
@@ -1,8 +1,8 @@
 ﻿namespace APBD_Cw1_s30650.Models.Equipment;
 
-public class Laptop(string brand, string model, int diskspace, int memory, string processor, OperatingSystem system ,decimal price) : Equipment(brand, model, price)
+public class Laptop(string brand, string model, int diskSpace, int memory, string processor, OperatingSystem system ,decimal price) : Equipment(brand, model, price)
 {
-    public int DiskSpace { get; set; } = diskspace;
+    public int DiskSpace { get; set; } = diskSpace;
     public int Memory { get; set; } = memory;
     public string Processor { get; set; } = processor;
     public OperatingSystem System { get; set; } = system;
--- Models/Equipment/Projector.cs
+++ Models/Equipment/Projector.cs
@@ -1,6 +1,8 @@
 ﻿namespace APBD_Cw1_s30650.Models.Equipment;
 
-public class Projector
+public class Projector(string brand, string model, int brightness, string resolution, int contrastRatio, decimal price) : Equipment(brand, model, price)
 {
-    
+    public int Brightness { get; } = brightness;
+    public string Resolution { get; } = resolution;
+    public int ContrastRatio { get; } = contrastRatio;
 }
